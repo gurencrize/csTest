2f2b7fb8cbd841f4ad0c2e7bbb06adcf487a7d3c
crize <34015092+gurencrize@users.noreply.github.com>
2021-10-10 22:38:57 +0900

    今後勉強したいことの布石だけ貼っとく

---
+++ csTest/SwitchExpressionTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csTest
+{
+    class SwitchExpressionTest
+    {
+        // https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/operators/switch-expression
+    }
+}
