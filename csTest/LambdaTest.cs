660af9cbab3b8731fad02f411b5acbf450f81d5a
crize <34015092+gurencrize@users.noreply.github.com>
2021-10-25 23:32:08 +0900

    Create LambdaTest.cs
    学習目的がなさすぎて無理だった

---
+++ csTest/LambdaTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace csTest
+{
+    class LambdaTest
+    {
+        public static void Run()
+        {
+            // ラムダ式 https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/operators/lambda-expressions
+            // 主な用途は2種類
+            // デリゲートとしての利用(匿名メソッドの代用) (使い捨てのメソッドを作るときに使用する)
+            // Expression型に入れると式木として利用できる→式木ってなんだ…?
+
+            // デリゲートに関数を入れる目的で使用する時
+            // 値を返さない場合はActionデリゲートに入れる
+
+            // よくわかんない！
+
+            WriteLine();
+        }
+    }
+}
